919e60a06a06dc3429b58c500c339f058b4b89aa
Wisdom Okwen <wisdomokwen@gmail.com>
2025-06-21 11:53:57 -0700

    Homepage setup

---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export function Home() {
+    return (
+        <div className="container">
+            <h>I hate this already</h>
+        </div>
+    );
+}
+
+
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export function NavBar() {
+    return (
+        <div className="nav">
+            <p>Yeah fuck this shit</p>
+        </div>
+    );
+}
