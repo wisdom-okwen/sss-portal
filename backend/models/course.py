86caf11cfa1ddbd7934dc411e9746a10eb5fc33c
Prince Millidah <princemillidah21@gmail.com>
2025-07-07 00:45:46 -0400

    create course entity

---
+++ backend/models/course.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import List
+from datetime import time
+from ..models.user import User
+
+
+class CourseTime(BaseModel):
+    day: str
+    start_time: time
+    end_time: time
+
+
+class Course(BaseModel):
+    id: int | None = None
+    course_name: str = ""
+    course_code: str = ""
+    course_description: str = ""
+    course_lecturer: User | None = None
+    course_capacity: int = 0
+    course_time: List[CourseTime] = []
+    course_members: List[User] = []
+    credits: int = 0
+    department_id: int | None = None
+    prerequisites: List[int] = []
+    course_type: str = ""
+    num_enrolled: int = 0
+    location: str = ""
+    semester: str = ""
